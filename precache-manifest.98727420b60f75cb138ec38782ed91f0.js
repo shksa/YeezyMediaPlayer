9a75af635f46d6bfcf649ad5140b9f43be4e4158
sreekar <sreekar339@gmail.com>
2018-10-24 00:40:44 +0530

    Updates

---
+++ precache-manifest.98727420b60f75cb138ec38782ed91f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c49febc48585999dcaf4dc35de4b4b12",
+    "url": "/YeezyMediaPlayer/static/media/spinner.c49febc4.svg"
+  },
+  {
+    "revision": "701d9ae61f6317bfa69f",
+    "url": "/YeezyMediaPlayer/static/js/runtime~main.701d9ae6.js"
+  },
+  {
+    "revision": "9a343f75441bf89781c9",
+    "url": "/YeezyMediaPlayer/static/js/main.9a343f75.chunk.js"
+  },
+  {
+    "revision": "047e88384fc99ccdd168",
+    "url": "/YeezyMediaPlayer/static/js/1.047e8838.chunk.js"
+  },
+  {
+    "revision": "9a343f75441bf89781c9",
+    "url": "/YeezyMediaPlayer/static/css/main.f48e597f.chunk.css"
+  },
+  {
+    "revision": "99ec08b2e80719b4f6e985dd0afa9ebd",
+    "url": "/YeezyMediaPlayer/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/yeezyMediaPlayer/precache-manifest.dc996123f7b8036664042120b5524e03.js"
+  "/YeezyMediaPlayer/precache-manifest.98727420b60f75cb138ec38782ed91f0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/yeezyMediaPlayer/index.html", {
+workbox.routing.registerNavigationRoute("/YeezyMediaPlayer/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
