1201c209573d26fa13940a2b5c592efa98112999
sreekar <sreekar339@gmail.com>
2018-10-24 00:25:45 +0530

    Updates

---
+++ precache-manifest.dc996123f7b8036664042120b5524e03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c49febc48585999dcaf4dc35de4b4b12",
+    "url": "/yeezyMediaPlayer/static/media/spinner.c49febc4.svg"
+  },
+  {
+    "revision": "5f5383eeb4715f2c932a",
+    "url": "/yeezyMediaPlayer/static/js/runtime~main.5f5383ee.js"
+  },
+  {
+    "revision": "2fec5083b87f2eadd605",
+    "url": "/yeezyMediaPlayer/static/js/main.2fec5083.chunk.js"
+  },
+  {
+    "revision": "27b5de2af5092961c61e",
+    "url": "/yeezyMediaPlayer/static/js/1.27b5de2a.chunk.js"
+  },
+  {
+    "revision": "2fec5083b87f2eadd605",
+    "url": "/yeezyMediaPlayer/static/css/main.f48e597f.chunk.css"
+  },
+  {
+    "revision": "10d536011437995b335ac72a8155f56b",
+    "url": "/yeezyMediaPlayer/index.html"
+  }
+];
